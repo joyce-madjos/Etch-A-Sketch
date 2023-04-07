839230eed56140ac9fbfaa18d62c216c51055a5a
joyce-madjos <madjos.joyce.arapan@gmail.com>
2023-04-07 20:01:53 +0800

    Added feature to  ask user for grid line & border

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
-var gridContainer = document.getElementById("grid-container");
+let gridContainer = document.getElementById("grid-container");
+let randomColor = document.getElementById("random-color");
+let blackColor = document.getElementById("black-button");
+let whiteColor = document.getElementById("white-button");
+let deleteColors = document.getElementById("delete-button");
+let color = document.getElementById("color");
+let textColor = document.getElementById("text-color");
+let mouseColor = document.getElementById("mouse-color");
+let withBorder = document.getElementById("with-border");
+let withOutBorder = document.getElementById("w-o-border");
 
-for (let index = 1; index <= 16; index++) {
-  for (let a = 1; a <= 16; a++) {
-    var tiles = document.createElement("div");
-    // tiles.innerHTML = a;
-    tiles.classList.add("box");
-    tiles.style.width = "5%";
-    tiles.style.height = "6.3%";
-    gridContainer.appendChild(tiles);
-  }
+
+// Default grid
+setGridLines("16");
+
+// Creation of grid
+function setGridLines (gridLinesNumber){
+  gridContainer.innerHTML = ""
+  for (let index = 1; index <= gridLinesNumber; index++) {
+    for (let a = 1; a <= gridLinesNumber; a++) {
+      let boxes = document.createElement("div");
+      boxes.classList.add("box");
+      boxes.style.width = "auto";
+      boxes.style.height = "auto";
+      boxes.style.flexBasis =  "calc(100% / " + gridLinesNumber + ")";
+      gridContainer.appendChild(boxes);
+    }
+    }
 }
 
-const slider = document.querySelector(".slider");
-const output = document.getElementById("slider-value");
+// Get user's desired number of grid
+function getNumber() {
+  let numberInput = document.getElementById("set-grid-boxes");
+  let output = document.getElementById("grid-value");
+  let number = numberInput.value;
+  if(number > 100){
+    output.textContent = "Max: 100";
+    number = 100;
+  }
+  else{
+    output.textContent = number + "x" + number;
+  }
 
-slider.addEventListener("input", () => {
-  output.textContent = slider.value + "x" + slider.value;
-});
+  // Pass the user's desired number to create the grid
+  setGridLines(number);
+  
+}
 
-// // const slider = document.querySelector('.slider');
-// const thumb = slider.querySelector('.slider-thumb');
-
-// slider.addEventListener('mouseover', () => {
-//   const randomColor = getRandomColor();
-//   thumb.style.backgroundColor = randomColor;
-// });
+// Responsible for coloring the grid
+function hoverBoxes(color,boxes) {
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("mouseover", function() {
+      this.style.backgroundColor = color;
+    });
+  }
+}
 
 function getRandomColor() {
   const letters = "0123456789ABCDEF";
@@ -34,58 +63,55 @@ function getRandomColor() {
   }
   return color;
 }
-// ---------------
-
-let randomColor = document.getElementById("random-color");
-let blackColor = document.getElementById("black-button");
-let whiteColor = document.getElementById("white-button");
-let deleteColors = document.getElementById("delete-button");
-const boxes = document.querySelectorAll(".box");
-var color = document.getElementById("color");
-var textColor = document.getElementById("text-color");
-var mouseColor = document.getElementById("mouse-color");
-
-hoverBoxes();
-
-function hoverBoxes(color) {
-  boxes.forEach((boxes) => {
-    boxes.addEventListener("mouseover", () => {
-      boxes.style.backgroundColor = color;
-    });
-  });
-}
 
 blackColor.addEventListener("click", () => {
-  hoverBoxes("black");
+  let boxes = document.querySelectorAll(".box");
+  hoverBoxes("black", boxes);
   mouseColor.innerHTML = "Black";
   color.style.background = "black";
 });
 
 randomColor.addEventListener("click", () => {
+  let boxes = document.querySelectorAll(".box");
   mouseColor.innerHTML = "Random";
-  mouseColor.style.background= "white";
-  textColor.style.background= "white";
-//   linear-gradient(to right, #ff0000, #ff7f00, #ffff00, #00ff00, #0000ff, #4b0082, #8f00ff);
   color.style.background = "linear-gradient(to right, #ff0000, #ff7f00, #ffff00, #00ff00, #0000ff, #4b0082, #8f00ff)";
   boxes.forEach((boxes) => {
     boxes.addEventListener("mouseover", () => {
       const randomColors = getRandomColor();
       boxes.style.backgroundColor = randomColors;
+      // hoverBoxes(randomColors);
     });
   });
 });
 
 whiteColor.addEventListener("click", () => {
+  let boxes = document.querySelectorAll(".box");
   mouseColor.innerHTML = "White";
-  hoverBoxes("white");
+  hoverBoxes("white", boxes);
   color.style.background = "white";
 });
 
 deleteColors.addEventListener("click", () => {
+  let boxes = document.querySelectorAll(".box");
   mouseColor.innerHTML = "";
-  hoverBoxes("white");
+  hoverBoxes("white", boxes);
   color.style.background = "white";
   boxes.forEach((boxes) => {
     boxes.style.backgroundColor = "white";
   });
 });
+
+withBorder.addEventListener("click", () => {
+  let boxes = document.querySelectorAll(".box");
+  boxes.forEach(box => {
+    box.style.border = "1px solid black";
+  });
+});
+
+withOutBorder.addEventListener("click", () => {
+  let boxes = document.querySelectorAll(".box");
+  boxes.forEach(box => {
+    box.style.border = "none";
+  });
+});
+
